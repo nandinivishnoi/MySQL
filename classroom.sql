27aef75b78b56285d0428287989607e86f40e4c3
Nandini Vishnoi <nandinivishnoi2016@gmail.com>
2025-02-10 12:50:57 +0530

    Add files via upload
    just created the fist database and table to get started with mysql

---
+++ classroom.sql
@@ -0,0 +1,20 @@
+create database college;
+use college;
+create table student(
+id int primary key,
+name varchar(50),
+age int not null);
+
+insert into student values(1,"Nandini",20);
+insert into student values(2,"Manish",23);
+insert into student values(3,"Aman",22);
+insert into student values(4,"Vishank",22);
+insert into student values(5,"Khushbu",21);
+
+select * from student;
+
+create database if not exists college;   /* will only create database if not created already*/
+drop database if exists company;   /*will nor give error but warning*/
+
+show databases;   /*shows all the databases*/
+show tables;   /*shows all tables of current database*/
